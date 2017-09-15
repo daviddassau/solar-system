aaed7f2aed59ec245eb3c07685ba3638281bc859
David Dassau <todieahero@hotmail.com>
2017-09-14 20:11:46 -0500

    it's broken

--- dist/app.js
+++ dist/app.js
@@ -8,9 +8,14 @@ document.getElementById("planetButtons").addEventListener('click', (event) => {
 	let planetSelected;
 	if(event.target.id === 'planetButtons'){
 		planetSelected = 'mercury';
+	} else {
+		planetSelected = event.target.id;
 	}
 	document.getElementById("planetsDiv").innerHTML = AllDaPlanets[planetSelected]();
 });
+
+
+// Error is saying: AllDaPlanets[planetSelected] is not a function
 },{"./planet-generator":10}],2:[function(require,module,exports){
 "use strict";
 
